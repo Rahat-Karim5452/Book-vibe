b5ff9c0aeed20a89781ce3756b84ff50029682b2
Rahat Karim <rahatkarim5452@gmail.com>
2026-09-24 02:01:43 +0600

    Listed Book and wishlist added

--- src/app/listed-books/page.tsx
+++ src/app/listed-books/page.tsx
@@ -4,16 +4,45 @@ import ListedBooksCard from "@/components/shared/ListedBooksCard";
 import { BooksContext } from "@/context/BooksContext";
 import IBookType from "@/types/typs";
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 const ListedBookspage = () => {
   const { readBooks, wishList } = useContext(BooksContext);
+  const { sortby, setSortBy } = useState<"rating" | "pages" | "year">("rating");
+
+  const sortBooks = (books: IBookType[]) => {
+    const sortedBooks = [...books];
+    if (sortby === "rating") {
+      sortedBooks.sort((a, b) => b.rating - a.rating);
+    } else if (sortby === "pages") {
+      sortedBooks.sort((a, b) => b.totalPages - b.totalPages);
+    } else if (sortby === "year") {
+      sortedBooks.sort((a, b) => b.yearOfPublishing - b.yearOfPublishing);
+    }
+  };
+  const sortedReadBooks = sortBooks(readBooks);
+  const sortedWishlist = sortBooks(wishList);
   return (
     <div className="w-full">
       <div className="max-w-7xl mx-auto px-4 py-10">
         <h2 className="bg-amber-100 rounded-2xl py-10 font-bold text-4xl text-center mb-8">
           Listed Books
         </h2>
+
+        <div className="text-center">
+          <select
+            value={sortby}
+            onChange={(e) =>
+              setSortBy(e.target.value as "rating" | "pages" | "year")
+            }
+            className="select select-success "
+          >
+            <option disabled={true}>Sort by</option>
+            <option value={"rating"}>Rating</option>
+            <option value={"year"}>Publised Year</option>
+            <option value={"pages"}>Number of Pages</option>
+          </select>
+        </div>
 
         <div className="tabs tabs-lift w-full">
           <input
@@ -23,8 +52,8 @@ const ListedBookspage = () => {
             aria-label={`Read Books (${readBooks.length})`}
           />
           <div className="tab-content bg-base-100 border-base-300 p-6">
-            {readBooks.length > 0 ? (
-              readBooks.map((book: IBookType) => {
+            {sortedReadBooks.length > 0 ? (
+              sortedReadBooks.map((book: IBookType) => {
                 return <ListedBooksCard key={book.bookId} book={book} />;
               })
             ) : (
@@ -42,8 +71,8 @@ const ListedBookspage = () => {
             defaultChecked
           />
           <div className="tab-content bg-base-100 border-base-300 p-6 space-y-5">
-            {wishList.length > 0 ? (
-              wishList.map((book: IBookType) => {
+            {sortedWishlist.length > 0 ? (
+              sortedWishlist.map((book: IBookType) => {
                 return <ListedBooksCard key={book.bookId} book={book} />;
               })
             ) : (
